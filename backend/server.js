5c3e2d07f4e7ae4b555349bb355ebf20c200ec03
SivaprasadK12 <sivaprasadk1206@gmail.com>
2025-07-14 20:14:53 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
     cors({
-    origin: process.env.CLIENT_URL,
+    origin: 'https://expense-tracker-backend-v7a7.onrender.com',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
