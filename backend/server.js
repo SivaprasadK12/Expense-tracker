441da2d65ff37c2f46f4a86c1971b5f23c74ebcb
SivaprasadK12 <sivaprasadk1206@gmail.com>
2025-07-15 11:38:07 +0530

    Fix: fixed backend

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ const express = require("express")
 const cors = require("cors")
 const path = require("path")
 const connectDB = require("./config/db")
-
 const app = express()
+
 app.use(express.json());
+
 
 const allowedOrigins = [
   "http://localhost:3000",
@@ -25,6 +26,16 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://expense-tracker-mauve-mu.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
